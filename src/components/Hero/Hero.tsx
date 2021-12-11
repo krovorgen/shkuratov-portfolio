d2b4459836b4fc3a1e2870c8a8b5da11f28f6060
maksim <krovorgen@gmail.com>
2021-12-11 18:03:52 +0300

    Добавлен тултип

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,11 @@
 import React, { FC } from 'react';
+import Tippy from '@tippyjs/react';
 
 import { heroData } from '../../data/heroData';
 
 import styles from './style.module.scss';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light-border.css';
 
 export const Hero: FC = () => {
   return (
@@ -10,7 +13,27 @@ export const Hero: FC = () => {
       <div className="container">
         <h1 className={styles.title}>
           Product digital <span>designer.</span> <br />
-          Помогу <span>улучшить UX/CX</span> <br />
+          Помогу{' '}
+          <span>
+            улучшить{' '}
+            <Tippy
+              content={
+                <div className={styles.tooltip}>
+                  <mark>UX</mark> — опыт и ощущения людей от использования Вашего <span>цифрового</span> продукта.{' '}
+                  <mark>CX</mark> — эмоции человека от взаимодействия с Вашим <span>бизнесом.</span>
+                </div>
+              }
+              theme="light-border"
+            >
+              <mark className={styles.faq}>
+                UX/CX
+                <svg>
+                  <use xlinkHref={`./images/sprite.svg#faq`} />
+                </svg>
+              </mark>
+            </Tippy>
+          </span>{' '}
+          <br />
           Вашего <span>продукта</span> или услуги <br />
         </h1>
         <ul className={styles.items}>
