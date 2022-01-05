72bd074c27bb14d5d6ffc77234c64ab4e9127e10
maksim <krovorgen@gmail.com>
2022-01-05 11:25:12 +0300

    правки

--- src/pages/About/WatchAll/WatchAll.tsx
+++ src/pages/About/WatchAll/WatchAll.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import styles from './WatchAll.module.scss';
+import { HashLink } from 'react-router-hash-link';
 
 export const WatchAll = () => {
   return (
     <div className={styles.root}>
       <div className="container">
-        <Link to="/" className={styles.link}>
+        <HashLink className={styles.link} smooth to="/#keys">
           <span>
             Смотреть все кейсы
             <svg>
               <use xlinkHref={`./images/sprite.svg#arrow-right`} />
             </svg>
           </span>
-        </Link>
+        </HashLink>
       </div>
     </div>
   );
