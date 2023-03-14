a77e66d88a22f3996b6c2b2e08cf03c62b286e82
maksim <krovorgen@gmail.com>
2023-03-14 20:01:17 +0300

    update version react + update cv

--- src/components/ScrollToTop/ScrollToTop.tsx
+++ src/components/ScrollToTop/ScrollToTop.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-export const ScrollToTop: FC = ({ children }) => {
+export const ScrollToTop: FC<{ children: React.ReactNode }> = ({ children }) => {
   const location = useLocation();
   useEffect(() => {
     window.scrollTo(0, 0);
